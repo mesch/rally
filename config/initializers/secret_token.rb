47c316bdfbf03f9b853dad3072469456885f8a1a
Matthew Esch <matt@rallycommerce.com>
2011-05-03 16:49:43 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rally::Application.config.secret_token = '25d4fc2c08a44fd6c7b656f7b61b69bda4d688f73f1c1c6ec1a3c8533ee333bcaf5990c2b65303ea9390e594aea750d489fe1114329c324ea838e37e14a38f95'
